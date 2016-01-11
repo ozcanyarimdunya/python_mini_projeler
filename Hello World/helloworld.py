e2d86cca60a21a14bdd41e0d4b384845dc92f933
ozcaan11 <ozcanyd@gmail.com>
2016-01-11 23:35:07 +0200

    Initial commit

---
+++ Hello World/helloworld.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+
+"""
+    Pythonda diğer proglama dillerine nazaren daha az syntax kullanılır
+    Örneğin C de yazacağınız bir hello world uygulaması için;
+     # include<stdio.h>
+     # include<stdlib.h>
+     int main()
+     {
+        printf("Hello World");
+        system("pause");
+        return 0;
+     }
+
+     kodlarını yazmanız gerekmektedir.
+     Çok fazla kod satırı ve syntax özellikle büyük programlarda çok karmaşık olur
+     Derlenmesi, çalıştırılması buna bağlı olarak zaman almakta ve performans kaybı yaşatmaktadır
+
+     Gelin pythonda basit bir hello world uygulaması nasıl yazılır ona bakalım
+"""
+
+print "Hello World"
+
+"""
+    Ve uygulamamız tamamdır.
+    Sadece tek satırlık bir kod ile C de bu 8 satır yer kaplamıştı
+    Üstelik platform bağımsız çalışacak bir uygulama oldu
+    Output için extra bir dosya oluşturmadan .py dosyamızı istediğimiz işletim
+    sisteminde kullanabiliriz artık
+    İşletim sisteminde Python kurulu olması lazım
+    Linux tabanlı çoğu işletim sisteminde python default olarak kurulu gelmektedir
+    Yalnız windows ta kendiniz kurmanız lazım boyutu çok büyük değil max 50MB dır
+"""
