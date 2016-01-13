8b5e1a284e85378d76a8c9c77ddc0459e429f3b4
ozcaan11 <ozcanyd@gmail.com>
2016-01-14 00:12:28 +0200

    Eğitim Seti tamamdır

---
+++ Python Mini Eğitim Seti/Diziler/diziler.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+
+"""
+    Dizi içinde 0,1 veya 1 den fazla eleman bulunduran kümelerdir
+"""
+
+dizi_1 = [45, 13, 32, "özcan", "abc"]
+
+for i in dizi_1:
+    print i
+
+"""
+Output:
+
+45
+13
+32
+özcan
+abc
+
+Process finished with exit code 0
+"""
+
+# ---------------------------------------------------- #
+
+print dizi_1[0]     # 45
+
+print dizi_1[2]     # 32
---
+++ Python Mini Eğitim Seti/Sözlükler/sozlukler.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+
+"""
+    Sözlük içinde anahtar ve değer bulunduran bir nevi dizilerdir
+"""
+
+sozluk = {"anahtar_1": "değer_1", "anahtar_2": 3, "abc": "özcan" }
+
+print sozluk["anahtar_1"]   # değer_1
+print sozluk.keys()         # ['abc', 'anahtar_2', 'anahtar_1']
+print sozluk.values()       # ['özcan', 3, 'değer_1']
