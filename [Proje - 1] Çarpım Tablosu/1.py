5f2f59224e482a62751ead340a8bf863a70eddb1
ozcaan11 <ozcanyd@gmail.com>
2016-01-14 13:19:40 +0200

    Basit bir çarpım tablosu projesi

--- [Proje - 1] Çarpım Tablosu/1.py
+++ [Proje - 1] Çarpım Tablosu/1.py
@@ -5,27 +5,39 @@
 """
 
 print "-"*50, "\n"
-print " "*20, "HOSGELDINIZ", "\n"
-print "-"*50, "\n"
 
 def carpim(i, j, r):
     if r != -1:
         if i*j == r:
-            return "***** Doğru *****"
+            print "***** Doğru *****"
         else:
-            return "!!! Yanlış cevap %s olacaktı"%(i*j)
+            print "!!! Yanlış cevap %s olacaktı"%(i*j)
     else:
         exit(0)
 
-
-def baslangic():
-    for sayi_1 in range(1, 6):
-        for sayi_2 in range(0, 6):
+def basla(rng_1, rng_2):
+    for sayi_1 in range(rng_1, rng_2):
+        for sayi_2 in range(rng_1-1, rng_2):
             print "_"*50, "\n"
-            print "\t%d x %d kaça eşittir(çıkış = -1)"%(sayi_1, sayi_2)
-            sonuc = input("sonuc = ")
-            print carpim(sayi_1, sayi_2, sonuc)
+            print "\t%d x %d kaça eşittir? (çıkış = -1)"%(sayi_1, sayi_2)
+            sonuc = input("sonuc >> ")
+            carpim(sayi_1, sayi_2, sonuc)
 
 
 if __name__ == '__main__':
-    baslangic()
+    print "Hangi seviyeden başlamak istiyorsunu ?"
+    print "  1 - Kolay "
+    print "  2 - Orta "
+    print "  3 - Zor"
+    print "  4 - Çok zor"
+    svy = input(" >> ")
+    if svy == 1:
+        basla(1, 6)
+    if svy == 2:
+        basla(6, 11)
+    if svy == 3:
+        basla(11, 21)
+    if svy == 4:
+        basla(22, 100)
+
+# @ozcaan11
