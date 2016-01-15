97cb0a5dea8d58ce2040c567654b4432dde1d753
ozcaan11 <ozcanyd@gmail.com>
2016-01-15 02:39:12 +0200

    Basit bir çarpım tablosu projesi

--- [Proje - 1] Çarpım Tablosu/1.py
+++ [Proje - 1] Çarpım Tablosu/1.py
@@ -1,5 +1,9 @@
 # coding=utf-8
 
+import os
+import random
+from random import choice
+
 """
     Küçük çapta bir çarpım tablosu uygulaması
 """
@@ -13,31 +17,52 @@ def carpim(i, j, r):
         else:
             print "!!! Yanlış cevap %s olacaktı"%(i*j)
     else:
-        exit(0)
+        os.system("clear")
+        secim()
 
 def basla(rng_1, rng_2):
-    for sayi_1 in range(rng_1, rng_2):
-        for sayi_2 in range(rng_1-1, rng_2):
+    if rng_1 > 10:
+        x = 10
+    else:
+        x = 5
+    for i in range(0, x):
+        for j in range(0, x):
+            sayi_1 = random.randint(rng_1, rng_2)
+            sayi_2 = random.randint(rng_1, rng_2)
             print "_"*50, "\n"
             print "\t%d x %d kaça eşittir? (çıkış = -1)"%(sayi_1, sayi_2)
             sonuc = input("sonuc >> ")
             carpim(sayi_1, sayi_2, sonuc)
 
+            if i == 4 and j == 4:
+                print "\n*-- Bu bölüm bitti bir üst bölüme geçebilsiniz --*\n"
+                secim()
 
-if __name__ == '__main__':
-    print "Hangi seviyeden başlamak istiyorsunuz ?"
+def secim():
+    print "Hangi seviyeden başlamak istiyorsunuz (çıkış = -1) ?"
     print "  1 - Kolay "
     print "  2 - Orta "
     print "  3 - Zor"
     print "  4 - Çok zor"
+
     svy = input(" >> ")
+
     if svy == 1:
         basla(1, 6)
+
     if svy == 2:
-        basla(6, 11)
+        basla(6, 12)
+
     if svy == 3:
-        basla(11, 21)
+        basla(12, 25)
+
     if svy == 4:
-        basla(22, 100)
+        basla(25, 100)
+
+    if svy == -1:
+        exit(0)
+
+if __name__ == '__main__':
+    secim()
 
 # @ozcaan11
