a51ab218228344fd36ae4941afdb4bf2b836314b
ozcaan11 <ozcanyd@gmail.com>
2016-01-17 15:52:57 +0200

    Ligtvden Galatasaray ile ilgili haberleri çekme

---
+++ [Proje - 2] Ligtv'den Haber Çekme/galatasaray_haberleri.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+
+import feedparser
+
+"""
+    ligtv haberleri feed ile ayrıştırma
+    Öncelikle feedparser modülünü indirip kurmanız gerekli
+    Terminalde  pip install feedparser  yapıp çalıştırsanız yeterli olacaktır
+"""
+
+print ('''
+    ######################################
+    #                                    #
+    #    Ligtv Galatasaray Haberleri     #
+    #                                    #
+    ######################################
+''')
+haberler_galatasaray = feedparser.parse('http://www.ligtv.com.tr/rss/takim/galatasaray')
+
+i = 1
+for haber in haberler_galatasaray.entries:
+    print i, haber.title
+    print "  ", haber.guid, "\n"
+    i += 1
