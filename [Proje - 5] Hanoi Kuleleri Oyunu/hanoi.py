bd35d4c5261b242e53fb5f8eb78c408059e3ec7a
ozcaan11 <ozcanyd@gmail.com>
2016-01-17 21:28:10 +0200

    Hanoi kuleleri oyunu

---
+++ [Proje - 5] Hanoi Kuleleri Oyunu/hanoi.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+"""
+ n: disk sayısı,
+ s: kaynak,
+ t: hedef,
+ b: tampon
+
+"""
+
+
+def hanoi(n, s, t, b):
+        assert n > 0
+        if n == 1:
+            print 'move', s, 'to', t
+        else:
+            hanoi(n-1, s, b, t)
+            hanoi(1, s, t, b)
+            hanoi(n-1, b, t, s)
+
+if __name__ == '__main__':
+    hanoi(7, 1, 2, 1)
