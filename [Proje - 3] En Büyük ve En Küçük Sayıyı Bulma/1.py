09ec008696efd4729fa6232840c509547fce470c
ozcaan11 <ozcanyd@gmail.com>
2016-01-17 18:07:04 +0200

    Kulanıcının girdiği sayıların en büyüğü ve en küçüğünü bulma

---
+++ [Proje - 3] En Büyük ve En Küçük Sayıyı Bulma/1.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+
+"""
+    Girilen sayılardan en büyük ve en küçük sayıları bulma
+"""
+
+sayilar = []
+
+flag = True
+while flag:
+    try:
+        a = input("Sayıları girin(çıkış için -1): ")
+        if a == -1:
+            flag = False
+        else:
+            sayilar.append(float(a))
+    except SyntaxError:
+        print "HATA: Yalnızca sayı girin !"
+    except NameError:
+        print "HATA: Yalnızca sayı girin !"
+
+try:
+    en_buyuk = sayilar[0]
+    en_kucuk = sayilar[0]
+    ortalama = sayilar[0]
+except IndexError:
+    print "\nEn az bir sayı girmelisiniz"
+    en_buyuk = "yok"
+    en_kucuk = "yok"
+    ortalama = "yok"
+finally:
+    toplam = 0
+
+for sayi in sayilar:
+    if sayi > en_buyuk:
+        en_buyuk = sayi
+    if sayi < en_kucuk:
+        en_kucuk = sayi
+    toplam += sayi
+    ortalama = toplam/len(sayilar)
+
+print "\nen büyük sayı = {}\n" \
+        "en küçük sayı = {}\n" \
+        "ortalama      = {}".format(en_buyuk, en_kucuk, ortalama)
