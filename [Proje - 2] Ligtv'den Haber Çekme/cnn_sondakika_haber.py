973a00c39d44efde1a7acf304e0bbc7a99067e4b
ozcaan11 <ozcanyd@gmail.com>
2016-01-18 14:26:27 +0200

    Ligtv ve cnn rss feedparser kullanımı

---
+++ [Proje - 2] Ligtv'den Haber Çekme/cnn_sondakika_haber.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+
+import feedparser
+
+"""
+    cnn'den son dakika haberlerini çekme
+"""
+url = "http://www.cnnturk.com/feed/rss/news"
+haberler = feedparser.parse(url)
+
+i = 0
+for haber in haberler.entries:
+    i += 1
+    print i
+    print haber.title
+    print haber.link
+    print haber.summary
+    print "\n"
