5f8154d166c4bf4e6ba78b0b7524c9a4db0e27d5
ozcaan11 <ozcanyd@gmail.com>
2016-01-18 15:13:44 +0200

    Zar oyunu

---
+++ [Proje - 10] Zar Oyunu/zar.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+
+import random
+import time
+
+"""
+    2 adet zar atılsın ve her iki zarın değeri de 6 olduğunda program dursun.
+    İki zar da 6 gelene kadar kaç kez zar atıldığını bildiren programı yazınız.
+"""
+
+
+i = 1
+while True:
+    zar_1 = random.randint(1, 6)
+    zar_2 = random.randint(1, 6)
+
+    if zar_1 == 6 and zar_2 == 6:
+        print """Deneme {}:\t({},{}) *** """.format(i, zar_1, zar_2)
+        time.sleep(2)
+        break
+
+    print """Deneme {}:\t({},{}) """.format(i, zar_1, zar_2)
+    i += 1
+    time.sleep(0.5)
+
+print """\n*** {}. denemede (6,6) geldi ***""".format(i)
