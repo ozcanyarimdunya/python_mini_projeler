3225140555394900a566462697c3189e5265f57f
ozcaan11 <ozcanyd@gmail.com>
2016-01-21 20:41:05 +0200

    Eskişehir saatlik hava durumu bilgisi Sıcaklık çekmede sorun var !

--- [Proje - 11] Eskişehir Saat Başı Hava Durumu/hava.py
+++ [Proje - 11] Eskişehir Saat Başı Hava Durumu/hava.py
@@ -8,7 +8,7 @@ import re, urllib
 url = "http://www.mgm.gov.tr/tahmin/default.aspx?s=ESKISEHIR"
 html = urllib.urlopen(url).read()
 
-regex_sicaklik = '<th class="sicak1" style="font-weight:bold; ' \
+regex_sicak = '<th class="sicak1" style="font-weight:bold; ' \
                  'color:red; background-color:#ecf4f8;">(.*)</th>'
 
 regex_nem = '<th  class="nem1" style="color:#4247b8; ' \
@@ -20,7 +20,7 @@ regex_ruzgar = '<th class="ruzgarham1" style="color:#731d75; ' \
 regex_saat = '<th style="color:#8095a4; background-color:#ecf4f8; ' \
              'font-weight:normal;">([\d+][\d+]:00)</th>'
 
-comp_sicaklik = re.compile(regex_sicaklik)
+comp_sicak = re.compile(regex_sicak)
 comp_nem = re.compile(regex_nem)
 comp_ruzgar = re.compile(regex_ruzgar)
 comp_saat = re.compile(regex_saat)
@@ -30,8 +30,7 @@ nem = []
 ruzgar = []
 saat = []
 
-for i in re.findall(comp_sicaklik, html):
-    print i
+for i in re.findall(comp_sicak, html):
     sicaklik.append(i)
 
 for i in re.findall(comp_nem, html):
@@ -49,4 +48,7 @@ ESKİŞEHİR SAAT BAŞI HAVA DURUMU
 -------------------------------
 """
 for i in range(0, len(saat)):
-    print "saat: {},\tsıcaklık: ?,\tnem: {},\truzgar: {}".format(saat[i], nem[i], ruzgar[i])
+    print "saat: {},\tsıcaklık: {},\tnem: {},\truzgar: {}".format(saat[i], sicaklik[i], nem[i], ruzgar[i])
+
+
+# sıcaklık çekmede bir hata oldu sonra düzeltilecek
