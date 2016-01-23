a2dfcdb1d11f07fb22fe19a9da41c60cd279e272
Özcan Yarımdunya <ozcanyd@gmail.com>
2016-01-23 02:42:27 +0200

    Update zar.py

--- [Proje - 10] Zar Oyunu/zar.py
+++ [Proje - 10] Zar Oyunu/zar.py
@@ -1,13 +1,12 @@
 # coding=utf-8
 
-import random
-import time
-
 """
     2 adet zar atılsın ve her iki zarın değeri de 6 olduğunda program dursun.
     İki zar da 6 gelene kadar kaç kez zar atıldığını bildiren programı yazınız.
 """
 
+import random
+import time
 
 i = 1
 while True:
