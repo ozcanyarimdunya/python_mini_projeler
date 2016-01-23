195677b1cc794f20dc8455482c7ff26002b3be11
ozcaan11 <ozcanyd@gmail.com>
2016-01-23 05:18:26 +0200

    Asal Sayı Bulma

---
+++ [Proje - 15] Asal Sayı Bulma/1.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+"""
+    Girdiğimiz bir sayıya kadar ki asal olan bütün sayıları listeleme
+"""
+
+deger = input("Kaça kadar ki asal sayıları arıyorsunuz? : ")
+asal = []
+
+for i in range(2, deger):
+    flag = False
+    for j in range(2, i):
+        if i % j == 0:
+            flag = True
+            break
+    if not flag:
+        asal.append(i)
+
+for i in asal:
+    print i
+
+print "\n0 - {} arasında toplam {} tane asal sayı vardır.".format(deger, len(asal))
