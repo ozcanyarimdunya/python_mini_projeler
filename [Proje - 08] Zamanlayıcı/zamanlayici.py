d0b9d5baa74a0cd5a4aade8efd9dd608dc2404ae
Özcan Yarımdunya <ozcanyd@gmail.com>
2019-01-06 22:11:39 +0300

    Zamanlıyıcı İçin Ufak Eklemeler #2

--- [Proje - 08] Zamanlayıcı/zamanlayici.py
+++ [Proje - 08] Zamanlayıcı/zamanlayici.py
@@ -7,15 +7,19 @@
 
 import time
 
-zaman = time.localtime()
-yil = zaman[0]
-ay = zaman[1]
-gun = zaman[2]
-saat = zaman[3]
-dakika = zaman[4]
-saniye = zaman[5]
+while True:
+    zaman = time.localtime()
+    yil = zaman[0]
+    ay = zaman[1]
+    gun = zaman[2]
+    saat = zaman[3]
+    dakika = zaman[4]
+    saniye = zaman[5]
+    
+    time.sleep(1)
 
-print("""
-    tarih: {}/{}/{}
-    saat : {}:{}:{}
-    """.format(gun, ay, yil, saat, dakika, saniye))
+    print("""
+        tarih: {}/{}/{}
+        saat : {}:{}:{}
+        """.format(gun, ay, yil, saat, dakika, saniye))
+ 
